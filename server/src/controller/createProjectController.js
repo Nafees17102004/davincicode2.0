aca268823f5263de89c927764c158167e533972e
Ameen <mohamed.kaamil.ameen@gmail.com>
2025-09-29 16:26:22 +0530

    insert field api updated

--- server/src/controller/createProjectController.js
+++ server/src/controller/createProjectController.js
@@ -1,7 +1,7 @@
-const createProjectService = require('../service/createProjectService');
+const createProjectService = require("../service/createProjectService");
 
-const insertProject = async (req,res) =>{
-    try {
+const insertProject = async (req, res) => {
+  try {
     const projects = Array.isArray(req.body) ? req.body : [req.body];
     const results = [];
     const errors = [];
@@ -11,16 +11,20 @@ const insertProject = async (req,res) =>{
 
       // validation
       if (!pCode || !pName || !pLanguageId || !pStatus) {
-        errors.push({ index, error: "All fields are required", employee: project });
+        errors.push({
+          index,
+          error: "All fields are required",
+          employee: project,
+        });
         continue; // skip this one, continue with next
       }
 
       try {
         const [result] = await createProjectService.insertProject(
-          pCode, 
-          pName, 
-          pLanguageId, 
-          pStatus, 
+          pCode,
+          pName,
+          pLanguageId,
+          pStatus,
           pInactiveReason
         );
         results.push({ ...project, dbResult: result });
@@ -34,86 +38,109 @@ const insertProject = async (req,res) =>{
       addedCount: results.length,
       failedCount: errors.length,
       addedProjects: results,
-      failedProjects: errors
+      failedProjects: errors,
     });
-
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-}
+};
 
-const viewProjectDetails=async(req,res)=>{
-    try {
-        const result=await createProjectService.viewProjectDetails();
-        res.status(200).json(result);
-    } catch (err) {
-        res.status(500).json({message:err.message});
-    }
+const viewProjectDetails = async (req, res) => {
+  try {
+    const result = await createProjectService.viewProjectDetails();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
+};
 const insertLanguage = async (req, res) => {
-    try {
-      const language = Array.isArray(req.body) ? req.body : [req.body];
-      const results = [];
-      const errors = [];
+  try {
+    const language = Array.isArray(req.body) ? req.body : [req.body];
+    const results = [];
+    const errors = [];
 
-      for (const [index, lang] of language.entries()) {
-        const { lName, lStatus, lInactiveReason } = lang;
-        if (!lName || !lStatus) {
-          errors.push({ index, error: "All fields are required", language: lang });
-          continue; // skip this one, continue with next
-        }
-        try {
-          const result = await createProjectService.insertLanguage(lName, lStatus, lInactiveReason);
-          results.push({ ...lang, dbResult: result });
-        } catch (dbErr) {
-          errors.push({ index, error: dbErr.message, language: lang });
-        }
+    for (const [index, lang] of language.entries()) {
+      const { lName, lStatus, lInactiveReason } = lang;
+      if (!lName || !lStatus) {
+        errors.push({
+          index,
+          error: "All fields are required",
+          language: lang,
+        });
+        continue; // skip this one, continue with next
       }
-      res.status(201).json({
-        message: "Processing completed",
-        addedCount: results.length,
-        failedCount: errors.length,
-        addedLanguages: results,
-        failedLanguages: errors
-      });
-    } catch(err){
-      res.status(500).json({error: err.message})
+      try {
+        const result = await createProjectService.insertLanguage(
+          lName,
+          lStatus,
+          lInactiveReason
+        );
+        results.push({ ...lang, dbResult: result });
+      } catch (dbErr) {
+        errors.push({ index, error: dbErr.message, language: lang });
+      }
     }
-  };
+    res.status(201).json({
+      message: "Processing completed",
+      addedCount: results.length,
+      failedCount: errors.length,
+      addedLanguages: results,
+      failedLanguages: errors,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-  const getLanguage=async(req,res)=>{
-    try{
-        const result=await createProjectService.getLanguage();
-        res.status(200).json(result);
-    }catch(err){
-      res.status(500).json({message:err.message});
-    }
-  };
+const getLanguage = async (req, res) => {
+  try {
+    const result = await createProjectService.getLanguage();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-  const getFieldTypes=async(req,res)=>{
-    try {
-      const result=await createProjectService.getFieldTypes();
-      res.status(200).json(result);
-    } catch (err) {
-        res.status(500).json({message:err.message});
+const getFieldTypes = async (req, res) => {
+  try {
+    const result = await createProjectService.getFieldTypes();
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const insertFieldTypes = async (req, res) => {
+  try {
+    const { fieldName } = req.body;
+    if (!fieldName) {
+      res.status(401).json({ message: "All fields are required" });
     }
-  };
-  
-  const getSnippetById=async(req,res)=>{
-    try {
-      const s_id=req.params.s_id;
-      const result = await createProjectService.getSnippetById(s_id);
-      res.status(200).json(result);
-    } catch (err){
-      res.status(500).json({message:err.message});
-    }
-  };
+    const result = await createProjectService.insertFieldTypes(fieldName);
+    res
+      .status(201)
+      .json({ message: "Field types added successfully", data: result });
+  } catch (error) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const getSnippetById = async (req, res) => {
+  try {
+    const s_id = req.params.s_id;
+    const result = await createProjectService.getSnippetById(s_id);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 module.exports = {
-    insertProject,
-    viewProjectDetails,
-    insertLanguage,
-    getLanguage,
-    getFieldTypes,
-    getSnippetById
-}
+  insertProject,
+  viewProjectDetails,
+  insertLanguage,
+  getLanguage,
+  getFieldTypes,
+  getSnippetById,
+  insertFieldTypes,
+};
