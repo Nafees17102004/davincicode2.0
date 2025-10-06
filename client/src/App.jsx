5beb45047bcb7f5addb1f75193ac301714a9ad84
Ameen <mohamed.kaamil.ameen@gmail.com>
2025-10-06 12:31:20 +0530

    login route updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import ProjectPage from './pages/ProjectPage/ProjectPage'
-import LanguagePage from './pages/LanguagePage/LanguagePage';
-import ModulePage from './pages/ModulePage/ModulePage';
-import ViewProjectsPage from './pages/ViewProjectsPage/ViewProjectsPage';
-import ViewLanguagePage from './pages/ViewLanguagePage/ViewLanguagePage';
-import AddModulePage from './pages/ModulePage/AddModulePage'
-import FieldTypePage from './pages/FieldTypePage/FieldTypePage';
-import SnippetPage from './pages/SnippetPage/SnippetPage';
-import ViewFieldPage from './pages/ViewFieldPage/ViewFieldPage';
+import ProjectPage from "./pages/ProjectPage/ProjectPage";
+import LanguagePage from "./pages/LanguagePage/LanguagePage";
+import ModulePage from "./pages/ModulePage/ModulePage";
+import ViewProjectsPage from "./pages/ViewProjectsPage/ViewProjectsPage";
+import ViewLanguagePage from "./pages/ViewLanguagePage/ViewLanguagePage";
+import AddModulePage from "./pages/ModulePage/AddModulePage";
+import FieldTypePage from "./pages/FieldTypePage/FieldTypePage";
+import SnippetPage from "./pages/SnippetPage/SnippetPage";
+import ViewFieldPage from "./pages/ViewFieldPage/ViewFieldPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<LanguagePage />} />
       <Route path="/project" element={<ProjectPage />} />
       <Route path="/module/:pCode" element={<ModulePage />} />
