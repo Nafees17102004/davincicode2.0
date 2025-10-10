03203c4c374d6d7a9ae0c889ded8916b01494eef
ganeshj2004 <jaisankarganesh156@gmail.com>
2025-10-10 16:39:08 +0530

    BE update

---
+++ server/src/repository/tabRepository.js
@@ -0,0 +1,48 @@
+const pool = require("../config/dbConfig");
+
+const tabRepository = {
+  insertTab: async (projectId, pageId, tabName, tabImageId, createdUser) => {
+    try {
+      const query = "CALL SP_INSERT_TAB_TABLE(?, ?, ?, ?, ?)";
+      const [rows] = await pool.query(query, [
+        projectId,
+        pageId,
+        tabName,
+        tabImageId,
+        createdUser,
+      ]);
+      const message = rows?.[0]?.[0]?.message;
+      if (message.toLowerCase().includes("error")) {
+        return {
+          success: false,
+          message: message,
+        };
+      }
+      return {
+        success: true,
+        message: message,
+      };
+    } catch (err) {
+      console.error(
+        "Repository Error (insertTab):",
+        err.message || err.sqlMessage
+      );
+      throw new Error(err.message || err.sqlMessage);
+    }
+  },
+  getTabImg: async (tabId) => {
+    try {
+      const query = "CALL SP_GET_TAB_IMAGE(?)";
+      const [rows] = await pool.query(query, [tabId]);
+      return rows[0];
+    } catch (err) {
+      console.error(
+        "Repository Error (GetTabImg):",
+        err.message || err.sqlMessage
+      );
+      throw new Error(err.message || err.sqlMessage);
+    }
+  },
+};
+
+module.exports = tabRepository;
--- server/src/routes/createProjectRoutes.js
+++ server/src/routes/createProjectRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const createProjectController = require("../controller/createProjectController");
 const controller = require("../controller/codeGenController");
 const listOfValuesController = require("../controller/listOfValuesController");
+const tabController = require("../controller/tabController");
 
 // Get request
 router.get("/projects", createProjectController.viewProjectDetails);
@@ -15,7 +16,9 @@ router.get("/getLovs", listOfValuesController.getLov);
 router.get("/getLov/:l_id", listOfValuesController.getLov);
 router.get("/getListOfValuesDetails", controller.getListOfValuesDetails);
 router.get("/getListOfValuesDetail/:lovId", controller.getListOfValuesDetails);
-router.get("/get-lov-with-det/:lovId", listOfValuesController.getLovWithDet)
+router.get("/get-lov-with-det/:lovId", listOfValuesController.getLovWithDet);
+router.get("/getTabImgs", tabController.getTabImg);
+router.get("/getTabImg/:tabId", tabController.getTabImg);
 // Post request
 router.post("/add-project", createProjectController.insertProject);
 router.post("/insertModule/:project_id", controller.insertModule);
@@ -23,6 +26,7 @@ router.post("/add-lang", createProjectController.insertLanguage);
 router.post("/add-field-type", createProjectController.insertFieldTypes);
 router.post("/insertSnippet", controller.insertSnippet);
 router.post("/insert-lov", listOfValuesController.insertLov);
+router.post("/insert-tab", tabController.insertTab);
 
 // Put Request
 router.put("/update-lov/:l_id", listOfValuesController.updateLov);
