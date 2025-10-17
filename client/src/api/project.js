73fde39ae541aeebd6d1e52a97b025bf0dd7f773
ganeshj2004 <jaisankarganesh156@gmail.com>
2025-10-17 12:38:02 +0530

    BE update

---
+++ client/src/api/project.js
@@ -0,0 +1,10 @@
+// import api from "./axiosInstance";
+
+// const ProjectAPI = {
+//   insertProject: (data) => api.post("/add-project", data),
+//   viewProjects: () => api.get("/projects"),
+//   getProjectByCode: (pCode) => api.get(`/getProjectDetails/${pCode}`),
+//   insertModule: (pCode, data) => api.post(`/insertModule/${pCode}`, data),
+// };
+
+// export default ProjectAPI;
---
+++ client/src/api/snippet.js
@@ -0,0 +1,8 @@
+// import api from "./axiosInstance";
+
+// const SnippetAPI = {
+//   insertSnippet: (data) => api.post("/insertSnippet", data),
+//   getSnippetById: (id) => api.get(`/getSnippetById/${id}`),
+// };
+
+// export default SnippetAPI;
